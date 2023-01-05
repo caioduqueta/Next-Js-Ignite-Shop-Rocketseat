2754a021012519e131d06821bd0b889a249081f4
caioduqueta <caio.duqueta@gmail.com>
2023-01-05 11:40:24 -0300

    Estrutura Visual

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,55 @@
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+
+import camiseta1 from '../assets/Camisetas/1.png'
+import camiseta2 from '../assets/Camisetas/2.png'
+import camiseta3 from '../assets/Camisetas/3.png'
+import camiseta4 from '../assets/Camisetas/4.png'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+  
   return (
-   <h1>Hello World</h1>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} width={520} height={480} alt="Camiseta1" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} width={520} height={480} alt="Camiseta2" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="Camiseta3" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta4} width={520} height={480} alt="Camiseta4" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
